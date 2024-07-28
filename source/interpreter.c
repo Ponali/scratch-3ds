d197c7554531ed6ef1f9cab4a3f303fc7190912d
TheWahlolly <justexobyte@gmail.com>
2024-07-28 16:01:27 +0300

    no major changes.

--- source/interpreter.c
+++ source/interpreter.c
@@ -10,7 +10,7 @@ static float spriteX = 0.0f;
 static float spriteY = 0.0f;
 static float spriteRot = 90.0f;
 
-static void renderProject(bool scr) {
+static void projectFrame(bool scr, u32 kDown) {
 	if(spriteX>240.0f) {
 		spriteX=240.0f;
 	} if(spriteX<-240.0f) {
@@ -20,7 +20,9 @@ static void renderProject(bool scr) {
 	} if(spriteY<-180.0f) {
 		spriteY=-180.0f;
 	} if(spriteRot>=360.0f) {
-		spriteRot=spriteRot-360.0f; //i would use the % operator here but it doesnt work on floats. yes, scratch rotation can have a decimal.
+		spriteRot=spriteRot-360.0f; //i would use the % operator here but it doesnt work on floats. yes, scratch rotation may not be a round number.
+	} if(spriteRot<0.0f) {
+		spriteRot=spriteRot+360.0f;
 	}
 	
 	if(initframe==true) {
@@ -32,17 +34,21 @@ static void renderProject(bool scr) {
 	C2D_DrawSprite(&sprite);
 	if(scr) {
 		C2D_SpriteSetPos(&sprite, spriteX/1.5f+200.0f, spriteY/1.5f+120.0f);
-		spriteRot++;
 	} else {
 		C2D_SpriteSetPos(&sprite, spriteX/1.5f+160.0f, spriteY/1.5f+120.0f);
-		spriteRot--;
 	}
 	C2D_SpriteSetRotationDegrees(&sprite, spriteRot-90.0f);
+	
 }
 
-//static void runBlock(int blockID) {
-//	switch(blockID) {
-//		case 1:
-//			uhh...
-//	}
-//}
+static void runBlock(int blockID) {
+	switch(blockID) {
+		case 1:
+			spriteX=spriteX+sin(spriteRot)*10;
+			spriteY=spriteY+cos(spriteRot)*10;
+		case 2:
+			spriteRot=spriteRot+15;
+		case 3:
+			spriteRot=spriteRot-15;
+	}
+}
