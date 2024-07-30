675e1dc2d3bfa99af3abd2509f4b4cd7a1c13a17
TheWahlolly <justexobyte@gmail.com>
2024-07-30 08:44:25 +0300

    made start start the project...thats pretty much it

--- source/interpreter.c
+++ source/interpreter.c
@@ -5,6 +5,7 @@
 #include "editor.c"
 
 static bool initframe=true;
+static int framecount=0;
 
 static C2D_Sprite sprite;
 static C2D_SpriteSheet sillySheet;
@@ -32,7 +33,7 @@ static void insertRuntimeBlock(struct Block newBlock){
 
 static void startProject(){
 	// must be ran when the green flag is clicked
-
+	
 	// get green flags and add it to runtime blocks
 	for(int i=0;i<blockMatrixSize;i++){
 		if(blockMatrixDynamic[i].id==47){ // corresponds to the "when flag clicked" block
@@ -46,37 +47,6 @@ static void startProject(){
 		srand(rand());
 	for(int i=0;i<(rand()%32)+16;i++)
 		rand();
-}
-
-static void projectFrame(bool scr, u32 kDown) {
-	if(spriteX>240.0f) {
-		spriteX=240.0f;
-	} if(spriteX<-240.0f) {
-		spriteX=-240.0f;
-	} if(spriteY>180.0f) {
-		spriteY=180.0f;
-	} if(spriteY<-180.0f) {
-		spriteY=-180.0f;
-	} if(spriteRot>=360.0f) {
-		spriteRot=spriteRot-360.0f; //i would use the % operator here but it doesnt work on floats. yes, scratch rotation may not be a round number.
-	} if(spriteRot<0.0f) {
-		spriteRot=spriteRot+360.0f;
-	}
-	
-	if(initframe==true) {
-		initframe = false;
-		sillySheet = C2D_SpriteSheetLoad("romfs:/sprites.t3x");
-		C2D_SpriteFromSheet(&sprite, sillySheet, 0);
-		C2D_SpriteSetCenter(&sprite, 0.5f, 0.5f);
-	}
-	C2D_DrawSprite(&sprite);
-	if(scr) {
-		C2D_SpriteSetPos(&sprite, spriteX/1.5f+200.0f, spriteY/1.5f+120.0f);
-	} else {
-		C2D_SpriteSetPos(&sprite, spriteX/1.5f+160.0f, spriteY/1.5f+120.0f);
-	}
-	C2D_SpriteSetRotationDegrees(&sprite, spriteRot-90.0f);
-	
 }
 
 static void runBlock(int blockID) {
@@ -105,5 +75,43 @@ static void projectTick(){
 	for(int i=0;i<runtimeBlocksSize;i++){
 		runBlock(runtimeBlocks[i].id);
 		runtimeBlocks[i]=(*runtimeBlocks[i].after);
+	}
+}
+
+static void projectFrame(bool scr, u32 kDown) {
+	framecount++;
+	if(framecount==2) {
+		framecount=0;
+		projectTick();
+	}
+	if(spriteX>240.0f) {
+		spriteX=240.0f;
+	} if(spriteX<-240.0f) {
+		spriteX=-240.0f;
+	} if(spriteY>180.0f) {
+		spriteY=180.0f;
+	} if(spriteY<-180.0f) {
+		spriteY=-180.0f;
+	} if(spriteRot>=360.0f) {
+		spriteRot=spriteRot-360.0f; //i would use the % operator here but it doesnt work on floats. yes, scratch rotation may not be a round number.
+	} if(spriteRot<0.0f) {
+		spriteRot=spriteRot+360.0f;
+	}
+	
+	if(initframe==true) {
+		initframe = false;
+		sillySheet = C2D_SpriteSheetLoad("romfs:/sprites.t3x");
+		C2D_SpriteFromSheet(&sprite, sillySheet, 0);
+		C2D_SpriteSetCenter(&sprite, 0.5f, 0.5f);
+	}
+	C2D_DrawSprite(&sprite);
+	if(scr) {
+		C2D_SpriteSetPos(&sprite, spriteX/1.5f+200.0f, spriteY/1.5f+120.0f);
+	} else {
+		C2D_SpriteSetPos(&sprite, spriteX/1.5f+160.0f, spriteY/1.5f+120.0f);
+	}
+	C2D_SpriteSetRotationDegrees(&sprite, spriteRot-90.0f);
+	if(kDown&KEY_START) {
+		startProject();
 	}
 }
